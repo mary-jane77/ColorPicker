d5a8e672d171a0fe67852dc8740b88870ddfe414
Elizabeth Tukaeva <elizabeth77531@gmail.com>
2020-08-29 03:13:33 +0300

    project

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-/* 
-    Use fetch() to load 1, then 5, then 25 colors from the HexBot
 
-    Use FlexBox to create a Responsive 5x5 Grid displaying the colors    
-*/
+function displayColors(colors) {
+    let myColorsHtml = colors.map(color => {
+        return `<div class="my-color" style="background-color: ${color.value}"></div>`
+    }).join('')
 
+    document.body.innerHTML = `<div class="my-colors">
+        ${myColorsHtml}
+    </div>`
+}
+
+const url = "https://api.noopschallenge.com/hexbot"
+
+async function getColors(howMany) {
+
+    let response = await fetch(`${url}?count=${howMany}`)
+    let data = await response.json()
+    let colors = data.colors
+
+    displayColors(colors)
+}
+
+getColors(100)
